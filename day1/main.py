1aa31487489ee02daf9f52e766587ae73bfa225b
Oyoshi <rafal.apriasz@gmail.com>
2021-12-02 11:16:26 +0100

    add file with input data, doc comment and replace pairwise custom function with builtin version

--- day1/main.py
+++ day1/main.py
@@ -1,27 +1,39 @@
-from itertools import islice, tee
+#!/usr/bin/python3
+import itertools as it
 
-def window_sliding_generator(iterable_input, size):
-    iterators = tee(iterable_input, size)
-    iterators = [islice(iterator, i, None) for i, iterator in enumerate(iterators)]
+def load_input(path):
+    with open(path) as f:
+        input_array = [int(val.rstrip('\r\n')) for val in f]
+    return input_array
+
+'''
+Transform some iterable e.g. list from [a0, a1, a2, a3, ...] into list of tuples.
+
+:param iterable iterable_input: iterable object which will be transformed into list of tuples
+:param int size: size of each tuple
+
+match size | 0 -> []
+           | 1 -> [a0, a1, a2, a3, ...] - original input
+           | 2 -> [(a0, a1), (a1, a2), (a2, a3), ...] - pairs
+           | 3 -> [(a0, a1, a2), (a1, a2, a3), (a2, a3, a4), ...] - triplets
+           ...
+'''
+def transform_by_window_sliding(iterable_input, size):
+    iterators = it.tee(iterable_input, size)
+    iterators = [it.islice(iterator, i, None) for i, iterator in enumerate(iterators)]
     yield from zip(*iterators)
 
-def pairwise(iterable_input):
-    a, b = tee(iterable_input)
-    next(b, None)
-    yield from zip(a, b)
-
 def count_increasing_elements(iterable_input):
-    pairs = [y-x for (x, y) in pairwise(iterable_input)]
+    pairs = [y-x for (x, y) in it.pairwise(iterable_input)]
     return len(list(filter(lambda elem: elem > 0, pairs)))
 
 def main():
-    # load data from a file, pass const object or use any other technique
-    input_array = []
+    input_array = load_input('input.txt')
     # PART 1
     increasing_elements_num_1 = count_increasing_elements(input_array)
     print(f'PART 1: {increasing_elements_num_1}')
     # PART 2
-    transformed_input = list(map(sum, (window_sliding_generator(input_array, 3))))
+    transformed_input = list(map(sum, (transform_by_window_sliding(input_array, 3))))
     increasing_elements_num_2 = count_increasing_elements(transformed_input)
     print(f'PART 2: {increasing_elements_num_2}')
 
