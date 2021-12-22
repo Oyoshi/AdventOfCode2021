ed43a1f489429ce14b9f28777fe66b8ae36ca20b
Oyoshi <rafal.apriasz@gmail.com>
2021-12-22 20:40:40 +0100

    solution for dat 12 - refactor to handle part 1 and part 2

--- day_12/main.py
+++ day_12/main.py
@@ -12,29 +12,32 @@ def reducer_callback(acc, cur):
     acc[cur[1]] = acc[cur[1]] + [cur[0]] if cur[1] in acc else [cur[0]]
     return acc
 
-def generate_all_paths(graph, source, destination, generate_all_paths_rec):
-    all_paths = []
-    visited = {k: False for k in graph}
-    generate_all_paths_rec(graph, source, destination, visited, all_paths)
-    return all_paths
-
-def generate_all_paths_rec_part1(graph, vertex, destination, visited, all_paths, path = []):
-    if vertex.islower():
-        visited[vertex] = True
-    path.append(vertex)
-    if vertex == destination:
-        all_paths.append(cp.deepcopy(path))
-    else:
-        for v in graph[vertex]:
-            if visited[v] == False:
-                generate_all_paths_rec_part1(graph, v, destination, visited, all_paths, path)
-                visited[v] = False
+def generate_all_paths(graph, source, destination, enable_doubles):
+    paths_ctr = 0
+    deque = [(source, set([source]), False)]
+    while len(deque) > 0:
+        node, seen, visited = deque.pop(0)
+        if node == destination:
+            paths_ctr += 1
+        else:
+            for direct_children in graph[node]:
+                if direct_children not in seen:
+                    seen_copy = cp.copy(seen)
+                    if direct_children.islower():
+                        seen_copy.add(direct_children)
+                    deque.append(((direct_children, seen_copy, visited)))
+                elif direct_children in seen and not visited and direct_children not in [source, destination] and enable_doubles:
+                    deque.append(((direct_children, seen, True)))
+    return paths_ctr
 
 def main():
     input_data = load_input('input.txt')
     # PART 1
-    all_paths_part1 = generate_all_paths(input_data, "start", "end", generate_all_paths_rec_part1)
-    print(f'PART 1: {len(all_paths_part1)}')
+    all_paths_part1 = generate_all_paths(input_data, "start", "end", False)
+    print(f'PART 1: {all_paths_part1}')
+    # PART 2
+    all_paths_part2 = generate_all_paths(input_data, "start", "end", True)
+    print(f'PART 2: {all_paths_part2}')
 
 if __name__ == '__main__':
     main()
